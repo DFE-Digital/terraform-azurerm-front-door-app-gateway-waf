11a98aefa295f67271b646475f56fba023b8d5b8
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-04-03 12:33:59 +0100

    Output resource group name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "environment" {
   value = local.environment
 }
+
+output "azurerm_resource_group_default" {
+  value       = local.resource_group
+  description = "Default Azure Resource Group"
+}
